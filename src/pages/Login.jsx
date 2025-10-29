0a02a5c078014c623db8a544de0bdbb187976e9e
pragyakushw-hash <prakushwah@bestpeers.com>
2025-10-29 17:50:20 +0530

    corrected css class

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
             id="text"
             value={user}
             onChange={(e) => setUser(e.target.value)}
-            className="shadow-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light"
+            className="shadow-xs bg-gray-50 border border-gray-300 text-red-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-xs-light"
             placeholder=""
             required
           />
@@ -48,7 +48,7 @@ const Login = () => {
         <div className="mb-5">
           <label
             for="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="block mb-2 text-sm font-medium text-red-800 dark:text-white"
           >
             Your password
           </label>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -34,7 +34,7 @@ const SignUp = () => {
             name="floating_email"
             id="floating_email"
             onChange={(e) => setEmail(e.target.value)}
-            className="text-input w-full px-2 py-2 text-sm text-red-500 bg-transparent border-b-2 border-gray-300 focus:border-blue-600 focus:outline-none"
+            className="block py-2.5 px-0 w-full text-sm text-red-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
           />
@@ -52,7 +52,7 @@ const SignUp = () => {
             name="floating_password"
             id="floating_password"
             onChange={(e) => setPassword(e.target.value)}
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block py-2.5 px-0 w-full text-sm text-red-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
           />
@@ -70,7 +70,7 @@ const SignUp = () => {
             name="floating_name"
             id="floating_name"
             onChange={(e) => setUserName(e.target.value)}
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block py-2.5 px-0 w-full text-sm text-red-800 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
           />
